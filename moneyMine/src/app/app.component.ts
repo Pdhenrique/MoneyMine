e4ed3ac0d328c8f0aaa95a9d7321448f269cb722
Pedro Santana <pedro.ana@nexti.com>
2024-01-26 11:38:56 -0300

    feat: add register page

--- moneyMine/src/app/app.component.ts
+++ moneyMine/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component'
+import { SideBarComponent } from "./components/side-bar/side-bar.component"
+import { RegisterComponent } from './pages/register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, SideBarComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
